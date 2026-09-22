dfc965323788fa4c3aa7e48091b66b824eaa5f78
yuchen015 <hejyunlin@gmail.com>
2026-09-22 21:05:28 +0800

    增加輸入身高迴圈

--- First.js
+++ First.js
@@ -1,8 +1,9 @@
 var weight=56;
 var height=168;
 
-var readline = require("readline-sync")
-//user input
+var readline = require("readline-sync")//user input
+
+//使用者輸入體重
 while (true){
     weight = readline.questionFloat('Your weight (10~200KG)?');
     if (weight < 10 || weight > 200){
@@ -12,15 +13,25 @@ while (true){
         break;
 }
 
-
+//使用者輸入身高
+while(true){
+    height = readline.questionFloat('Your weight (110~200CM)?');
+    if(height < 110 || height > 200){
+        console.log("Please 110~200!");
+        continue;
+    }
+    else
+        break;
+}
 
 var bmi = weight/((height/100)**2);
-console.log("Your BMI value is "+ bmi);
+console.log("Your BMI value is "+ bmi);//計算身高體重並輸出
 
+//函數test
 function test(){
     var weight=50;
     console.log("weight="+weight)
     return weight;
 }
 var result=test();
-console.log("weight="+weight)
+console.log("weight="+weight)//函數輸出
