569432e7fc40263f74a8a1d198e320837e5910cd
yuchen015 <hejyunlin@gmail.com>
2026-09-22 16:06:53 +0800

    新增迴圈、npm、新增組態

--- First.js
+++ First.js
@@ -1,4 +1,26 @@
 var weight=56;
 var height=168;
+
+var readline = require("readline-sync")
+//user input
+while (true){
+    weight = readline.questionFloat('Your weight (10~200KG)?');
+    if (weight < 10 || weight > 200){
+        console.log("Please 10~200!");
+        continue;
+    }else
+        break;
+}
+
+
+
 var bmi = weight/((height/100)**2);
 console.log("Your BMI value is "+ bmi);
+
+function test(){
+    var weight=50;
+    console.log("weight="+weight)
+    return weight;
+}
+var result=test();
+console.log("weight="+weight)
